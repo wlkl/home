be2a2364383b4feec41848510e36c69a8f66feea
wlkl <v.klyukvin@gmail.com>
2014-12-05 14:38:29 +0300

    add style and login

--- flask_home.py
+++ flask_home.py
@@ -19,6 +19,27 @@ def show_picture():
 def create_picture():
     if not session.get('logged_in'):
         abort(401)
+    return redirect(url_for('show_picture'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_picture'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_picture'))
 
 
 
