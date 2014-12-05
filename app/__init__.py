ebebbd80d1741ab1e42a1db11df7399c7044ff8c
wlkl <v.klyukvin@gmail.com>
2014-12-05 15:53:02 +0300

    point to return 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-__author__ = 'wlkl'
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='abra-kadabra',
+    USERNAME='wlkl',
+    PASSWORD='wen1k0'
+))
