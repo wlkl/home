35a85c4a9bae6af6b0d0c8f9743c28ce94fe163d
greper <v.klyukvin@gmail.com>
2014-12-17 09:36:16 +0300

    improve camera.py

--- home/camera.py
+++ home/camera.py
@@ -2,9 +2,9 @@ import time, datetime
 import picamera
 
 def make_image():
-    name = datetime.datetime.strftime('%d.%m.%y-%H:%M:%S') + '.jpg'
+    name = datetime.datetime.now().strftime('%d.%m.%y-%H:%M:%S') + '.jpg'
     with picamera.PiCamera() as camera:
-        camera.resolution(1024, 768)
+        camera.resolution = (1024, 768)
         camera.start_preview()
         time.sleep(2)
         camera.capture(name)
