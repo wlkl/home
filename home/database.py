5060918cd0f9c34eecc229cd42d7e86d45aa4810
wlkl <v.klyukvin@gmail.com>
2014-12-18 13:35:51 +0300

    change database

--- home/database.py
+++ home/database.py
@@ -1,14 +1,14 @@
 from home import app
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
-#from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///' + app.config['DATABASE'], convert_unicode=True)
 metadata = MetaData()
-#metadata.reflect(extend_existing=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
 
 def init_db():
-    metadata.create_all(bind=engine)
+    metadata.bind = engine
+    metadata.reflect(extend_existing=True)
+    metadata.create_all()
