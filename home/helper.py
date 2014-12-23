8ffeec703419d5f3b654bc82a51b7ce84a060456
wlkl <v.klyukvin@gmail.com>
2014-12-23 15:31:44 +0300

    improve Images

--- home/helper.py
+++ home/helper.py
@@ -7,7 +7,8 @@ def correct_bd():
     db_session.query(Image).delete()
     db_session.commit()
     for name in os.listdir(app.static_folder):
-        db_session.add(Image(name))
+        if name.endswith('.jpg'):
+            db_session.add(Image(name))
     db_session.commit()
     response = db_session.query(Image).all()
     return response
