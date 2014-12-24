712e5079db225b1c2a6dfddcc9be6e790171ca71
wlkl <v.klyukvin@gmail.com>
2014-12-24 12:40:42 +0300

    add delete function

--- home/database.py
+++ home/database.py
@@ -1,7 +1,6 @@
 from home import app
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///' + app.config['DATABASE'], convert_unicode=True)
 metadata = MetaData()
