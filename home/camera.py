e5390b648ffcf45dba117b5a03b04ba71c6af420
wlkl <v.klyukvin@gmail.com>
2014-12-26 14:34:33 +0300

    add favicon.ico

--- home/camera.py
+++ home/camera.py
@@ -3,18 +3,17 @@ import time
 import datetime
 from home import app
 
-#import picamera
+import picamera
 import shutil
 
 
 def make_image():
     name = datetime.datetime.now().strftime('%d.%m.%y-%H:%M:%S') + '.jpg'
     path = os.path.join(app.static_folder, name)
-#    with picamera.PiCamera() as camera:
-#        camera.resolution = (1024, 768)
-#        camera.start_preview()
-#        time.sleep(2)
-#        camera.capture(path)
-#        camera.stop_preview()
-    shutil.copyfile(os.path.join(app.static_folder, 'img.jpg'), os.path.join(app.static_folder, name))
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(path)
+        camera.stop_preview()
     return name
